bf4449d30131c7fb412846d073a0444e1f215f52
Harsh P Patel <harshppatel@2880@gmail.com>
2019-08-02 14:49:19 -0500

    saving data to localStorage

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,20 @@ export default {
   methods: {
     async getWeatherData() {
       await API.getWeather(this.location.lang, this.location.lat)
-        .then((res) => { this.forecast = res; });
+        .then((res) => {
+          this.forecast = res;
+          this.setLocalData();
+        });
+    },
+    setLocalData() {
+      // Saving location to localStorage
+      const location = {
+        lat: this.location.lat,
+        lang: this.location.lang,
+        address: this.address,
+      };
+      localStorage.setItem('location', JSON.stringify(location));
+      localStorage.setItem('forecast', JSON.stringify(this.forecast));
     },
   },
   mounted() {
@@ -32,21 +45,33 @@ export default {
       navigator.geolocation.getCurrentPosition((res) => {
         this.location.lat = res.coords.latitude;
         this.location.lang = res.coords.longitude;
-        API.getAddress(this.location.lat, this.location.lang)
-          .then((response) => { this.address = response.name; });
-        this.getWeatherData();
+        if (!localStorage.getItem('location')) {
+          API.getAddress(this.location.lat, this.location.lang)
+            .then((response) => { this.address = response.name; });
+        } else {
+          this.address = JSON.parse(localStorage.getItem('location')).address;
+        }
+        if (!localStorage.getItem('forecast')) {
+          this.getWeatherData();
+        } else {
+          this.forecast = JSON.parse(localStorage.getItem('forecast'));
+        }
       }, () => {
-        this.location.lat = '40.730610';
-        this.location.lang = '-73.935242';
-        this.address = 'New York City, NY';
-        this.getWeatherData();
-        // Save last location to storage and set that here if it exists.
+        const localLocation = localStorage.getItem('location');
+        this.location.lat = localLocation ? JSON.parse(localLocation).lat : '40.730610';
+        this.location.lang = localLocation ? JSON.parse(localLocation).lang : '-73.935242';
+        this.address = localLocation ? JSON.parse(localLocation).address : 'New York City, NY';
+        if (localStorage.getItem('forecast')) {
+          this.forecast = JSON.parse(localStorage.getItem('forecast'));
+        } else {
+          this.getWeatherData();
+        }
       });
     }
   },
-  // updated() {
-  //   this.getWeatherData();
-  // },
+  updated() {
+    this.setLocalData();
+  },
 };
 </script>
 
